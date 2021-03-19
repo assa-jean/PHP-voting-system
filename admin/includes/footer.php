5919f2a448fc31ecdbddbd0f06b85029165b3a8f
assa-jean <62913573+assa-jean@users.noreply.github.com>
2021-03-19 22:27:20 +0100

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Voting System</b>
+    </div>
+     <strong> &copy; 2020 ICTU SUG ELECTION MADE BY <a href="https://newtechnology4africa.com">NTA & <a href="https://eaglegroop.com">EAGLE GROOP</a></strong>
+</footer>
---
+++ admin/includes/menubar2.php
@@ -0,0 +1,13 @@
+<aside class="main-sidebar">
+  <!-- sidebar: style can be found in sidebar.less -->
+  <section class="sidebar">
+    
+    <!-- sidebar menu: : style can be found in sidebar.less -->
+   <h1> 
+       <img src="../images/liver-2.gif" width="200">
+
+      </h1>
+  </section>
+  <!-- /.sidebar -->
+</aside>
+<?php include 'config_modal.php'; ?>
